7b64353091b863c0b56d4b563be51a38ddcc2b04
Emre Kocaman <emrekocaman47@gmail.com>
2021-09-08 17:23:06 +0300

    First Commit

---
+++ app/src/main/java/com/demarkelabs/android_parsesdk_kotlin/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.demarkelabs.android_parsesdk_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.parse.ParseObject
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstObject = ParseObject("FirstClass")
+        firstObject.put("message","Hey ! First message from android. Parse is now connected")
+        firstObject.saveInBackground {
+            if (it != null){
+                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
+            }else{
+                Log.d("MainActivity","Object saved.")
+            }
+        }
+    }
+}
